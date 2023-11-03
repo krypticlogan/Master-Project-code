dbf6fdeda643b99cb7f12ce0241559e1bec8fe0a
Logan <logan.zchjones@gmail.com>
2023-11-03 00:03:32 -0700

    mostly done

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-var key;
 var mode = 0;
 var gameMode = 0;
 var circleGame, mazeGame, keyboardGame;
@@ -7,22 +6,35 @@ const GAMEBOARD_HEIGHT = 500;
  //buttons
  var instructionExit;
  var restart;
- var backButton = createButton("Back");
+var keyboard, maze, circleG;
+var backButton;
+//keyboard variables
+var currentWord = "";
+var word;
+var words;
+var letterIndex;
+var wordIndex;
+let userInput;
+let lastKey;
+let displaying = [];
+var userWord;
 
 function setup() {
   createCanvas(displayWidth-20, displayHeight-140);
   colorMode(RGB);
+  backButton = createButton("Back");
 //game boards - for display
   circleGame = createGraphics(GAMEBOARD_LEN, GAMEBOARD_HEIGHT);
   mazeGame = createGraphics(GAMEBOARD_LEN, GAMEBOARD_HEIGHT);
   keyboardGame = createGraphics(GAMEBOARD_LEN,GAMEBOARD_HEIGHT);
 
-  keyboardGame.background(0,0,0);
+  userInput = "";
+  userWord = createDiv(userInput);
 
 
   // circleGame.background(0,0,0);
   circleMode(circleGame); 
-  keyboardMode(keyboardGame);
+  keyboard = new keyboardMode(keyboardGame);
   mazeMode(mazeGame);
 
 }
@@ -33,6 +45,7 @@ function draw() {
   if(mode == 0) {
     createHomeGui();
     loadGame();
+    
   }
 
 
@@ -119,6 +132,20 @@ function createGameGui(gameMode){ //GAME GUI
   // instructionExit.mousePressed();
   switch(gameMode){
     case 1: image(keyboardGame,displayWidth/2-625, displayHeight/2-300);
+      
+      var display = createDiv(userInput);
+      display.position(width/2,height/2);
+      display.size(50,10);
+      display.hide();
+      displaying.push(display);
+      for(let i = 0; i < displaying.length; i++){
+        displaying[i].show();
+      }
+      
+      if(userInput === word){
+        wordIndex++;
+        word = nextWord(words, wordIndex);
+      }
     break;
     case 2: image(mazeGame,displayWidth/2-625, displayHeight/2-300);
     break;
@@ -134,6 +161,7 @@ function back(){
     backButton.remove();
     }
 }
+
 function loadGame(){
   if(mouseIsPressed){
     let x = mouseX;
@@ -161,62 +189,116 @@ function loadGame(){
   }
 }
 
-var userInput = "";
-function keyTyped(){
-  switch(gameMode){
-    case 1: return;
-    case 2: return;
-    case 3: 
-    userTyping(key);
-  }
-}
-var userLast = '';
-
-function userTyping(key){
-    userLast = key;
-  }
-
 function keyboardMode(g){
 //game layout
+  g.background(255,255,255);
   g.textAlign(CENTER);
   g.push();
   g.fill(30,70,100);
   g.textSize(20);
-  // if(keyIsPressed){
-  //   g.background(255,255,255);
-  //   g.updatePixels();
-  // }
-  //game logic starts here
-  var wordBank = ["up", "bye", "bear", "teach", "better", "monster" ];
-  let level = 1;
-  let won = false;
-  let word = wordBank[level-1];
-  
-  let display = g.text(word, GAMEBOARD_LEN/2, GAMEBOARD_HEIGHT/2);
+
+  g.frameRate(60);
+
+  // generateWord();
+  let drawing = new drawKeyboardGame(g);
+
+  g.pop()
 
   }
 
-// function displayWord(word, g){
-//   return;
-// }
+//   var word;
+//   var words;
+//   var letterIndex;
+//   var wordIndex;
+// let userInput = "";
+// let lastKey;
+// let displaying = [];
 
-function playKeyboardGame(lvl){//open play
-  let length = word.length;
-  let nextLvl = lvl+1;
-  for(let i = 0; i < length; i++){// for each letter in current word
-    let letter = word.charAt(i);
+
+  function drawKeyboardGame(g){
+    // generateWord();
+    letterIndex = 0; // Index of the current letter being typed
+    words = ["apple", "banana", "cherry", "date", "elderberry"];
     
-    switch(userLast){
-      case userLast === letter: return;
-      // lvl++;
-      // break;
-      default: return;
-      // break;
+    var startTime = millis();
+    var score = 0;
+    var timer = 30;
+    wordIndex = 0;
+
+    // let word = words[wordIndex];
+    word = nextWord(words, wordIndex);
+    g.text(word, GAMEBOARD_LEN/2, GAMEBOARD_HEIGHT/2 + 70);
+
+    //   // Display user input with appropriate colors
+    // for (let i = 0; i < word.length; i++) {
+    //   g.fill(i < letterIndex ? (lastKey === word[i]) ? color(0, 255, 0) : color(255, 0, 0) : color(0));
+    //   g.text(word[i], GAMEBOARD_LEN / 2 - (word.length / 2 - i) * 20, GAMEBOARD_HEIGHT / 2 + 40);
+    //   }
+
+//       var userWord = createDiv(userInput);
+      // userWord.position(GAMEBOARD_LEN/2,GAMEBOARD_HEIGHT/2);
+      // userWord.size(20,5);
+      // userWord.hide();
+      // displaying.push(userWord);
+      // for(let i = 0; i < displaying.length; i++){
+      //   displaying[i].show();
+      // }
+
+        // Display score and timer
+    g.textSize(16);
+    g.fill(0);
+    g.text("Score: " + score, 50, 20);
+    let remainingTime = max(timer - int((millis() - startTime) / 1000), 0);
+    g.text("Time: " + remainingTime, GAMEBOARD_LEN - 50, 20);
+
+      // Check if the word is completed
+    if (letterIndex >= word.length) {
+      score++;
+      wordIndex++;
+      if (wordIndex >= words.length) {
+        wordIndex = 0;
+      }
+      currentWord = nextWord(words, wordIndex);
     }
 
+      // Check for game over
+  if (remainingTime <= 0) {
+    g.background(220);
+    g.textSize(32);
+    g.fill(0);
+    g.text("Game Over", GAMEBOARD_LEN / 2, GAMEBOARD_HEIGHT / 2 - 20);
+    g.text("Your Score: " + score, GAMEBOARD_LEN / 2, GAMEBOARD_HEIGHT / 2 + 20);
+    g.noLoop();
   }
 }
-//close play
+
+  function nextWord(array, index){
+    let wordIndex = index;
+    let words = array;
+    let word = words[wordIndex];
+    return word; 
+    
+  }
+
+
+
+// function keyPressed() {
+//   let word = nextWord(words, wordIndex);
+//   lastKey = key;
+//   if (keyCode >= 65 && keyCode <= 90) { // Check if it's a valid letter key
+//     let currentLetter = word[letterIndex];
+//     if (key === currentLetter) {
+//       letterIndex++;
+//     }
+//   }
+
+// }
+function keyPressed(){
+  lastKey = key;
+  userInput+=lastKey;
+  letterIndex++;
+  console.log(userInput);
+}
 
 function mazeMode(g){
   g.textAlign(CENTER);
@@ -234,93 +316,4 @@ function mazeMode(g){
   g.fill(30,70,100);
   g.textSize(20);
   g.text("CIRCLES HERE", GAMEBOARD_LEN/2, GAMEBOARD_HEIGHT/2);
-
-    //instructions
-  // fill(80,160,205); //lightblue
-  // square(displayWidth/2-175, displayHeight/2-220, 350);
-  // fill(40,40,205);
-  // rect(displayWidth/2-175, displayHeight/2-220,350,75);
-  // fill(255);
-  // textSize(20);
-  
-
-  // let title = text("Welcome to Roundabout Revelary!" /*,displayWidth/2, displayHeight/2-200*/); //Instructions Title
-  // // textSize(15);
-  // // fill(255);
-  // let description = text("The objective of this game is to press all of the circles that are of a blue shade.\n A timer has been set to 20 seconds. \nGood Luck!" 
-  //     /*,displayWidth/2, displayHeight/2-100*/); //Circle Instructions 
-  //     let circles = [];
-  //     var startTime;
-  //     let gameDuration = 20; // Game duration in seconds
-  //     // let beginButton;
-      
-  //       // if (mode === 0) {
-  //       //   createHomeGui();
-  //       // } else if (mode === 1) {
-  //         // createGameGui();
-  //         // if (circles.length === 0) {
-  //         //   beginButton.show();
-  //         // }
-  //         playCircleGame();
-}
-      
-      // function startCircleGame() {
-      //   mode = 1;
-      //   createCircles(10); // Create 10 circles for the game
-      // }
-      
-      // function beginCircleGame() {
-      //   beginButton.hide();
-      // }
-      
-      function playCircleGame() {
-        let currentTime = (millis() - startTime) / 1000; // Calculate elapsed time in seconds
-        if (currentTime >= gameDuration) {
-          endGame();
-        } else {
-          for (let i = circles.length - 1; i >= 0; i--) {
-            let circle = circles[i];
-            circle.display();
-            let d = dist(mouseX, mouseY, circle.x, circle.y);
-            if (d < circle.radius / 2 && circle.isBlue) {
-              circles.splice(i, 1); // Remove the clicked circle
-            }
-          }
-        }
-      }
-      
-      function endGame() {
-        mode = 0;
-        circles = [];
-        // Your end game logic here
-        // For example, show a game-over message.
-      }
-      
-      function createCircles(num) {
-        for (let i = 0; i < num; i++) {
-          let x = random(width);
-          let y = random(height);
-          let radius = random(20, 50);
-          let isBlue = random() < 0.5; // 50% chance of being blue
-          circles.push(new Circle(x, y, radius, isBlue));
-        }
-      }
-      
-      class Circle {
-        constructor(x, y, radius, isBlue) {
-          this.x = x;
-          this.y = y;
-          this.radius = radius;
-          this.isBlue = isBlue;
-        }
-      
-        display() {
-          if (this.isBlue) {
-            fill(0, 0, 255);
-          } else {
-            fill(255);
-          }
-          ellipse(this.x, this.y, this.radius);
-        }
-      }
-
+ }
