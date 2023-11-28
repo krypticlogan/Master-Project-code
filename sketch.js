284df887c3cb5d3a23e5911aed7d0d23cf98dc91
Logan <logan.zchjones@gmail.com>
2023-11-27 23:32:46 -0700

    all games

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-//global vars
 var mode = 0;
 var gameMode = 0;
 var circleGame, mazeGame, keyboardGame;
@@ -7,10 +6,10 @@ const GAMEBOARD_HEIGHT = 500;
 let keyboardImg;
 let circleImg;
 let mazeImg;
-
 let scaleX;
 let scaleY;
-
+var adjX;
+var adjY;
  //buttons
  var instructionExit;
  var restart;
@@ -32,8 +31,55 @@ let score = 0;
 let timer = 30;
 let remainingTime;
   
+//maze variables
+var player;
+let playerX;
+let playerY;
+let dragging = false;
+let targetX;
+let targetY;
+let startX;
+let startY;
+let boundingX;
+let boundingY;
+let errors = 0;
+let time;
+let won = false;
+let mazeLevel = 1;
 
+const maze1 = [
+  [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+  [1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1],
+  [1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1],
+];
 
+const maze2 = [
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1],
+  [1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1],
+];
+
+const maze3 = [
+  [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+  [0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+  [0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1],
+];
+
+const tileSize = 1250/maze1[0].length;
+let start, target;
+ //circle game vars
+ var circles = [];
+ let startCircleTime;
+ let gameDuration = 20; // Game duration in seconds;
+ let gameButtons = []; // Array to store game buttons
+var backButton;
+let circleScore;
 
 // let currentWord = "";
 // let wordIndex = 0;
@@ -42,17 +88,14 @@ let remainingTime;
 // let score = 0;
 // let timer = 30;
 
-
 function preload(){
   soundFormats('mp3');
   ding = loadSound('sounds/dingy.mp3')
   buzz = loadSound('sounds/buzzy.mp3')
-
   keyboardImg = loadImage('Images/keyboard game image.png');
   circleImg = loadImage('Images/Screen Shot 2023-11-14 at 7.14.44 PM.png');
   mazeImg = loadImage('Images/maze image.jpeg')
   }
-
 function setup() {
   createCanvas(displayWidth-20, displayHeight-140);
   textFont('Verdana');
@@ -64,13 +107,8 @@ function setup() {
   circleGame = createGraphics(GAMEBOARD_LEN, GAMEBOARD_HEIGHT);
   mazeGame = createGraphics(GAMEBOARD_LEN, GAMEBOARD_HEIGHT);
   keyboardGame = createGraphics(GAMEBOARD_LEN,GAMEBOARD_HEIGHT);
-
   userInput = "";
   startKeyboardTime = millis();
-
-
-
-
   // circleGame.background(0,0,0);
   circleMode(circleGame); 
   // startCircleGame();
@@ -78,10 +116,10 @@ function setup() {
   mazeMode(mazeGame);
   
   backButton = createButton("Back");
-
 }
-
 function draw() {
+  adjX = mouseX-(displayWidth/2-625);
+  adjY = mouseY-(displayHeight/2-300);
   clear();
   background(48,25,52);
   //home screen
@@ -90,8 +128,6 @@ function draw() {
     loadGame();
     
   }
-
-
   //game screens
   else {
     createGameGui(gameMode);
@@ -99,33 +135,28 @@ function draw() {
   }  
   
 }
-
 function createHomeGui() { //HOME GUI
   //header
   fill(19,68,50);//dark green
   rect(0,0,displayWidth, 100);
-
   //Title
   fill(255,255,255);
   textAlign(CENTER, TOP);
   textSize(40);
   text("Home", displayWidth/2, 30); //Home Text
-
   //welcome message
   textSize(25);
   text("Welcome to Motor Mender! \nThe program to help your motor functions.", displayWidth/2, 150);
-
   //app frames
   
   
   fill(255,255,255);
-  // image(keyboardImg,displayWidth/4-125, displayHeight/3,250,250);
-  square(displayWidth/4-125, displayHeight/3,250);
-  // image(mazeImg,(displayWidth)/2-125, displayHeight/3,250,250);
-  square((displayWidth)/2-125, displayHeight/3,250);
-  // image(circleImg,displayWidth*3/4-125, displayHeight/3,250,250);
-  square(displayWidth*3/4-125, displayHeight/3,250);
-
+  image(keyboardImg,displayWidth/4-125, displayHeight/3,250,250);
+  // square(displayWidth/4-125, displayHeight/3,250);
+  image(mazeImg,(displayWidth)/2-125, displayHeight/3,250,250);
+  // square((displayWidth)/2-125, displayHeight/3,250);
+  image(circleImg,displayWidth*3/4-125, displayHeight/3,250,250);
+  // square(displayWidth*3/4-125, displayHeight/3,250);
   //game descriptions
   fill(255,255,255);
   textSize(18);
@@ -133,24 +164,18 @@ function createHomeGui() { //HOME GUI
   text("Maze Game", displayWidth/2, displayHeight/3 + 280);
   text("Circle Game", displayWidth*3/4, displayHeight/3 + 280);
 }
-
-
 function createGameGui(gameMode){ //GAME GUI
   //header
   fill(19,68,50);//dark green
   rect(0,0, displayWidth, 100);
-
   //Title
   fill(255,255,255);
   textAlign(CENTER, TOP);
   textSize(40);
   text("TITLE", displayWidth/2, 30);
-
-
   //game board
   fill(255,255,255);
   rect(displayWidth/2-625, displayHeight/2-300, 1250, 500);
-
   // //instructions
   // fill(48,25,52); //lightblue
   // square(displayWidth/2-175, displayHeight/2-220, 350);
@@ -162,12 +187,10 @@ function createGameGui(gameMode){ //GAME GUI
   // textSize(15);
   // fill(255);
   // text("Place Instructions Here:", displayWidth/2, displayHeight/2-100); //Instructions Text
-
     backButton = createButton("Back");
     backButton.position(100,displayHeight/2+210);
     backButton.size(100,30);
     backButton.mousePressed(back);
-
   // var backButton;
   // var instructionExit;
   // var restart;
@@ -181,7 +204,6 @@ function createGameGui(gameMode){ //GAME GUI
     
     case 1: 
     // startKeyboardTime = millis();
-
    
     image(keyboardGame,displayWidth/2-625, displayHeight/2-300);
       keyboardGame.fill(30,70,100);
@@ -193,14 +215,59 @@ function createGameGui(gameMode){ //GAME GUI
       gameOver(keyboardGame);
     break;
     case 2: image(mazeGame,displayWidth/2-625, displayHeight/2-300);
+      mazeGame.background(0,0,0);
+      player = new Player(mazeGame, playerX, playerY, 'white');
+      if(dragging){
+        playerX = player.relX + player.offsetX;
+        playerY = player.relY + player.offsetY;
+ 
+      }
+      
+      
+      
+      // ^^ move player logic ^^
+      
+      
+      mazeGame.background(255);
+
+      drawMaze(mazeLevel);
+
+      if ((playerX >= targetX && playerX <= targetX + tileSize) && (playerY >= targetY && playerY <= targetY + tileSize)){
+        mazeLevel++;
+        findStartAndTarget(mazeLevel);
+        dragging = false;
+      }
+
+     
+
+
+      // Draw starting point
+      mazeGame.stroke('green')
+      mazeGame.fill(0, 255, 0); // Green
+      mazeGame.ellipse(start.x * tileSize + tileSize / 2, start.y * tileSize + tileSize / 2, tileSize * 0.8);
+
+      // Draw target
+      mazeGame.stroke('red')
+      mazeGame.fill(255, 0, 0); // Red
+      mazeGame.rect(target.x * tileSize, target.y * tileSize, tileSize, tileSize);
+
+      
+      //drawing player
+      mazeGame.fill('blue');
+      mazeGame.stroke('blue');
+      mazeGame.circle(playerX, playerY, player.r);
+      
+
+      //
+      mazeGame.fill('white');
+      mazeGame.circle(targetX,targetY,20);
         break;
     case 3: image(circleGame,displayWidth/2-625, displayHeight/2-300);
+        playCircleGame(circleGame);
     break;
   }
   
 }
-
-
 
 function back(){
   if(mode == 1){
@@ -213,14 +280,11 @@ function loadGame(){
   if(mouseIsPressed){
     let x = mouseX;
     let y = mouseY;
-
     //game 1
-
     if((x >= displayWidth/4-125 && x<= displayWidth/4+125)&&(y >= displayHeight/3 && y <= displayHeight/3+250)){
       mode = 1;
       gameMode = 1;
     }
-
     //game 2
     if((x >= displayWidth/2-125 && x <= displayWidth/2+125)&&(y >= displayHeight/3 && y <= displayHeight/3+250)) {
       mode = 1;
@@ -238,9 +302,6 @@ function loadGame(){
     }
   }
 }
-
-
-
 function keyboardMode(g){
 //game layout
   g.background(255,255,255);
@@ -250,7 +311,6 @@ function keyboardMode(g){
   g.textSize(40);
 
   g.frameRate(60);
-
   // generateWord();
   // let drawing = new drawKeyboardGame(g);
   words = ["apple", "banana", "cherry", "date", "elderberry", "abundant", "benevolent", "cacophony", "divergent", "eccentric", "facetious", "gargantuan", "hapless", "ineffable", "juxtapose", "kindle", "luminous", "mellifluous", "nebulous", "object", "palimpsest", "worm"];
@@ -258,9 +318,7 @@ function keyboardMode(g){
   wordIndex = 0;
   nextWord(words, wordIndex);
   g.pop()
-
   }
-
 //   var word;
 //   var words;
 //   var letterIndex;
@@ -270,6 +328,7 @@ function keyboardMode(g){
 // let displaying = [];
 
 
+
   function nextWord(array, index){
     let wordIndex = index;
     let words = array;
@@ -277,7 +336,6 @@ function keyboardMode(g){
     letterIndex = 0;
     currentWord = word; 
   }
-
 function checkWord(){
     // Check if the word is completed
   if (letterIndex >= currentWord.length) {
@@ -291,7 +349,6 @@ function checkWord(){
     ding.play();
   }
 }
-
 function displayWord(g){
   for (let i = 0; i < currentWord.length; i++) {
         if (i === letterIndex) {
@@ -318,7 +375,6 @@ if (remainingTime <= 1) {
   g.noLoop();
 }
 }
-
 function keyPressed() {
   if (keyCode >= 65 && keyCode <= 90) { // Check if it's a valid letter key
     let currentLetter = currentWord[letterIndex];
@@ -329,8 +385,6 @@ function keyPressed() {
     else{buzz.play();}
   }
 }
-
-
 function gameTimer(g){//   // Display score and timer
   g.textSize(26);
   g.noStroke();
@@ -352,10 +406,255 @@ function gameTimer(g){//   // Display score and timer
 
 
 // keyboard game ends
+
+//maze game starts here
+class Player {
+  constructor(g,pX,pY,pColor){
+    this.g = g;
+    this.x = pX;
+    this.y = pY;
+    this.color = pColor;
+    this.r = 40;
+    this.dragging = false;
+    this.touching = false;
+    this.relX = mouseX-(displayWidth/2-625);
+    this.relY = mouseY-(displayHeight/2-300);
+    this.offsetX = 0;
+    this.offsetY = 0;
+  }
+
+  // show(){
+  //   if(this.dragging){
+  //     this.g.fill(200,0,200);
+  //   } else {
+  //     this.g.fill(255,255,255);
+  //   }
+  //   this.g.circle(this.x,this.y,this.r);
+  // }
+}
+
+
+
+
+    
+  function findStartAndTarget(level) {
+            // Found an empty space, set it as the starting point
+            if(level === 1){
+              start = createVector(0, 0);    
+              target = createVector(6,3);
+            }
+            else if (level === 2){
+              start = createVector(1, 4);    
+              target = createVector(1, 0);
+            } 
+            else if(level === 3){
+              start = createVector(11,2);
+              target = createVector(0,0);
+            }
+            
+            startX = start.x *tileSize;
+            startY = start.y *tileSize;
+            // Find a random empty space for the target
+              targetX = target.x *tileSize;
+              targetY = target.y *tileSize;
+
+            restartPlayer();
+              return;
+            }
+
+    function drawMaze(level){
+      let maze;
+
+      if(level === 1){
+        maze = maze1;
+      }
+      else if (level === 2){
+        maze = maze2;
+      } 
+      else if(level === 3){
+        maze = maze3;
+      }
+        for (let i = 0; i < maze.length; i++) {
+          for (let j = 0; j < maze[i].length; j++) {
+            if (maze[i][j] === 1) {
+              mazeGame.stroke('black');
+              mazeGame.fill(0); // Wall
+              mazeGame.rect(j * tileSize, i * tileSize, tileSize, tileSize);
+              boundingX = j * tileSize;
+              boundingY = i * tileSize;
+              if ((playerX+player.r/2 >= boundingX && playerX-player.r/2 <= boundingX + tileSize) && (playerY+player.r/2 >= boundingY && playerY-player.r/2 <= boundingY + tileSize)){
+                restartPlayer();
+                dragging = false;
+              }
+              // console.log(i + " " + j);
+              // mazeGame.fill('pink');
+              // mazeGame.circle(boundingX,boundingY,20);
+            } else {
+              mazeGame.stroke('white');
+              mazeGame.fill(255); // Path
+              mazeGame.rect(j * tileSize, i * tileSize, tileSize, tileSize);
+            }
+          }
+        }
+    }
+
+    function restartPlayer(){
+      playerX = startX + tileSize/2;
+      playerY = startY + tileSize/2;
+    }
+
 function mazeMode(g){
+  g.textAlign(CENTER);
+  g.push();
+  g.background(0,0,0);
+  g.fill(30,70,100);
+  g.textSize(20);
 
+  // let player = createPlayer(g);
+  // player.changeColor(red);
+
+  // if(player.isSelected()){
+  //   // player.changeX(mouseX);
+  //   // player.changeY(mouseY);
+  //   return;
+  // } 
+  
+
+
+  //game logic starts here
+  findStartAndTarget(mazeLevel);
+}
+  
+//maze game ends here
+// circle game starts here
+ function circleMode(g) {
+  g.textAlign(CENTER);
+  g.push();
+  g.background(0,0,0);
+  g.fill(30,70,100);
+  g.textSize(20);
+  }
+ function startCircleGame() {
+   createCircles(20); // Create 10 circles for the game
+ }
+
+ function playCircleGame(g) {
+  // g.background(48, 25, 52);      
+   let currentTime = (millis() - startCircleTime) / 1000; // Calculate elapsed time in seconds
+   if (currentTime >= gameDuration) {
+     endGame();
+   } else {
+     for (let i = circles.length - 1; i >= 0; i--) {
+       let circle = circles[i];
+       circle.display(g);
+      let d = dist(adjX, adjY, circle.x, circle.y);
+       if (d < circle.radius / 2){
+        if(!circle.isBlue){
+          circle.isRed = true;
+          buzz.play();
+        }
+        else if(mouseIsPressed) {
+         circles.splice(i, 1); // Remove the clicked circle
+         g.background(0,0,0);      
+         ding.play();
+       }
+      }
+     }
+   }
+ }
+ 
+ function endGame() {
+   circles = [];
+   // Your end game logic here
+   // For example, show a game-over message.
+ }
+ 
+ const desiredSpacing = 2; // Adjust the desired spacing between circles
+ 
+ function createCircles(num) {
+   for (let i = 0; i < num; i++) {
+     let valid = false;
+     let x, y, radius, isBlue;
+
+     while (!valid) {
+       valid = true;
+       x = random(300, 1400);
+       y = random(100, GAMEBOARD_HEIGHT-100);
+       radius = random(20, 50);
+       isBlue = random() <= 0.4;
+
+
+       for (let circle of circles) {
+         let d = dist(x, y, circle.x, circle.y);
+         if (d < radius + circle.radius + desiredSpacing) {
+           valid = false;
+           break;
+         }
+       }
+     }
+ 
+     circles.push(new Circle(x, y, radius, isBlue));
+   }
+ }
+
+ class Circle {
+   constructor(x, y, radius, isBlue) {
+     this.x = x;
+     this.y = y;
+     this.radius = radius;
+     this.isBlue = isBlue;
+     this.isRed = false;
+   }
+
+   display(g) {
+     if (this.isBlue) {
+       g.fill(0, 0, 255);
+     }
+     else if (this.isRed){
+      g.fill(255,0,0);
+     } else {
+       g.fill(255);
+     }
+     g.ellipse(this.x, this.y, this.radius);
+   }
+ }
+
+
+ function mousePressed(){
+  if(gameMode == 1){
+    return;
+  }
+  else if (gameMode == 2){
+    let d = dist(player.relX,player.relY, playerX, playerY);
+      if (d < player.r / 2) {
+        dragging = true;
+        player.offsetX = player.x - player.relX;
+        player.offsetY = player.y - player.relY;
+            }
+            else{
+              dragging = false;
+            }
+    // return;
+    }
+  else if (gameMode == 3){
+    return;
+  }
 }
 
-function circleMode(g){
+function mouseReleased(){
+  if(gameMode == 1){
+    return;
+  }
+  else if (gameMode == 2){
+      dragging = false;
+  } 
+  else if (gameMode == 3){
+    return;
+  } 
 
-}
+ }
+
+ function restartCircles(){
+  circles = [];
+  circleScore = 0;
+ }
